8c1a8fd03d4edddca54b166c874aff3a6643c00a
Umang Patel <umang26patel@gmail.com>
2024-10-06 23:50:35 -0700

    update
    thread count

--- AndroidActivityLifecycle/app/src/main/java/com/example/androidactivitylifecycle/MainActivity.java
+++ AndroidActivityLifecycle/app/src/main/java/com/example/androidactivitylifecycle/MainActivity.java
@@ -7,48 +7,52 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int threadCounter = 0;
+
+    private int threadCounter = 0; // Global counter
+    private TextView counterTextView; // To display the counter
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counterTextView = findViewById(R.id.counter_text_view);
+        updateCounterDisplay();
+
+        Button buttonB = findViewById(R.id.start_activity_b_btn);
+        Button buttonC = findViewById(R.id.start_activity_c_btn);
+
+        buttonB.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, ActivityB.class);
+            intent.putExtra("counterValue", threadCounter); // Pass counter value
+            startActivityForResult(intent, 1); // Start Activity B
+        });
+
+        buttonC.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, ActivityC.class);
+            intent.putExtra("counterValue", threadCounter); // Pass counter value
+            startActivityForResult(intent, 2); // Start Activity C
+        });
     }
 
+    // Handle result from Activity B and C
     @Override
-    protected void onRestart() {
-        super.onRestart();
-        threadCounter += 1;
-
-        TextView threadCounterText = findViewById(R.id.thread_counter_txt);
-        threadCounterText.setText("ThreadCounter: " + String.valueOf(threadCounter));
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (data != null && data.hasExtra("updatedCounter")) {
+                threadCounter = data.getIntExtra("updatedCounter", 0); // Get updated counter value
+                updateCounterDisplay(); // Update the counter display
+            }
+        }
     }
 
-    public void onStartActivityB (View view) {
-        Intent activityBIntent = new Intent(MainActivity.this, ActivityB.class);
-        startActivity(activityBIntent);
-    }
-    public void onStartActivityC(View view) {
-        Intent activityCIntent = new Intent(MainActivity.this, ActivityC.class);
-        startActivity(activityCIntent);
-    }
-
-
-    public void onTriggerDialog (View view) {
-        new AlertDialog.Builder(this)
-                .setTitle("Dialog")
-                .setMessage("This is a Simple dialog")
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {}
-                })
-                .show();
-    }
-
-    public void onCloseApp (View view) {
-        finish();
-        System.exit(0);
+    // Function to update the counter display
+    private void updateCounterDisplay() {
+        counterTextView.setText("Thread Counter: " + threadCounter);
     }
 }
